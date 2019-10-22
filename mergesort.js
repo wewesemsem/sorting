fb8bf752024f71f28282e4b35c4ac14183f504d1
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2019-10-22 15:46:39 -0400

    merge function

--- mergesort.js
+++ mergesort.js
@@ -4,3 +4,22 @@ function split(wholeArr) {
   const secondHalf = wholeArr.slice(half);
   return [firstHalf, secondHalf];
 }
+
+function merge(arrayOne, arrayTwo) {
+  let mergedArr = [];
+
+  while (arrayOne.length > 0 || arrayTwo.length > 0) {
+    if (arrayOne[0] < arrayTwo[0] || arrayTwo.length === 0) {
+      mergedArr.push(arrayOne[0]);
+      //    console.log(arrayOne)
+      arrayOne.splice(0, 1);
+      //  console.log(arrayOne)
+    } else {
+      mergedArr.push(arrayTwo[0]);
+      //  console.log(arrayTwo)
+      arrayTwo.splice(0, 1);
+      //  console.log(arrayTwo)
+    }
+  }
+  return mergedArr;
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -4,3 +4,15 @@ describe('Split Array function', function() {
         expect(result).toEqual([[38, 27, 43, 3], [9, 82, 10]])
     })
 })
+
+describe('Merge function', function() {
+   it('is able to merge two sorted arrays into one array', function() {
+       const arr1 = [27, 38]
+       const arr2 = [3, 10]
+       expect(merge(arr1, arr2)).toEqual([3,10,27,38])
+
+       const arrOne = [3,27,38,43]
+       const arrTwo = [9,10,82]
+       expect(merge(arrOne, arrTwo)).toEqual([3,9,10,27,38,43,82])
+   }) 
+})
