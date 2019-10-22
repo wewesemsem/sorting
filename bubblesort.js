28285e44a2b5f129bd864149f76f7aa349a6cad0
Emma Curtis <ejcurtis@dons.usfca.edu>
2019-10-22 12:14:53 -0400

    completed workshop

--- bubblesort.js
+++ bubblesort.js
@@ -1,15 +1,19 @@
+const swapper = {}
+
 function bubbleSort(arr) {
 
     for (let i = 0; i < arr.length; i++) {
         for(let j = 0; j < arr.length; j++) {
             if (arr[i] < arr[j]) {
-                const temp = arr[j]
-                arr[j] = arr[i]
-                arr[i] = temp;
+              swapper.swap(i, j, arr)
             }
         }
     }
-
-    console.log('result', arr)
     return arr
+}
+
+swapper.swap = (i, j, arr) => {
+  const temp = arr[j]
+  arr[j] = arr[i]
+  arr[i] = temp;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,4 +1,18 @@
 describe('Bubble Sort', function() {
+    beforeAll(function () {
+      spyOn(swapper, 'swap').and.callThrough();
+    });
+
+    it('swap n2 times', function () {
+      bubbleSort([6, 8, 4, 9, 1]);
+      expect(swapper.swap.calls.count()).toBeLessThan(25);
+    });
+
+    // it('has bigO of one with an array of one', function (){
+    //   bubbleSort([1]);
+    //   expect(swapper.swap.calls.count()).toEqual(0);
+    // })
+
   it('handles an empty array', function() {
     expect(bubbleSort([])).toEqual([]);
   });
